c19b214f4f4e3fcdd9d5330b33e5a33de16dc1e0
punisher987 <87966433+punisher987@users.noreply.github.com>
2024-02-10 19:46:21 +0530

    Initial commit

---
+++ src/main/java/com/codewithdurgesh/blog/entities/Role.java
@@ -0,0 +1,35 @@
+package com.codewithdurgesh.blog.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Role {
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Id	
+	private int id;
+	
+	private String name;
+	
+}
---
+++ src/main/java/com/codewithdurgesh/blog/payloads/JwtAuthResponse.java
@@ -0,0 +1,27 @@
+package com.codewithdurgesh.blog.payloads;
+
+import lombok.Data;
+
+@Data
+public class JwtAuthResponse {
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public UserDto getUser() {
+		return user;
+	}
+
+	public void setUser(UserDto user) {
+		this.user = user;
+	}
+
+	private String token;
+	
+	private UserDto user;
+}
